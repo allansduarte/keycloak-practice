558807a7149bd47d8b2a8bfb60aa72504a779f35
Allan <allan.sduarte@gmail.com>
2021-03-29 20:08:34 -0300

    Fix logger

--- go-client/main.go
+++ go-client/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
 )
 
 var (
@@ -18,13 +19,13 @@ func main() {
 
 	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/myrealm")
 	if err != nil {
-		log.Fatalf(err)
+		log.Fatal(err)
 	}
 
 	config := oauth2.Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
-		Endpoint:     provider.Endpoint{},
+		Endpoint:     provider.Endpoint(),
 		RedirectURL:  "http://localhost:8081/auth/callback",
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
 	}
